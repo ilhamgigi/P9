82948c27a64922bf821b2e79dd337d52f4ced6b8
ilhamgigi <99956221+ilhamgigi@users.noreply.github.com>
2022-02-23 01:56:11 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,40 +1,47 @@
-var box;
+var sea,ship;
+var seaImg,shipImg;
 
-function setup() {
-  createCanvas(400,400);
-  box = createSprite(200,200,30,30);
+function preload(){
+  //uncomment the code to add animation to ship 
 
+  shipImg1 = loadAnimation("ship-1.png");
+  //shipImg1 = loadAnimation("ship-1.png");
+ //shipImg1 = loadAnimation("ship-1");
+ shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+ //shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
+  
+  seaImg = loadImage("sea.png");
 }
 
-function draw() 
-{
-   background(30);
+function setup(){
+  createCanvas(400,400);
+  background("blue");
 
-  if (keyIsDown(RIGHT_ARROW)) 
-  {
-    background("red")
+  // Moving background
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.velocityX = -5;
+  sea.scale=0.3;
+
+  
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25;
+  
+}
+
+function draw() {
+  background(0);
+  sea.velocityX = -3;
+
+  //uncomment code to reset the background
+  if(sea.x < 0){
+   // sea.x = 0;
+    //sea.x = sea.width;
+    sea.x = sea.width/8;
+    //sea.y = height;
   }
 
-    if (keyIsDown(LEFT_ARROW)) 
-  {
-    background("blue")
-    
-  }
  
-    if (keyIsDown(UP_ARROW)) 
-  {
-    background("green")
-   
-  }
-
-  if (keyIsDown(DOWN_ARROW)) 
-  {
-    background("yellow")
-  }
-
   drawSprites();
 }
-
-
-
-
